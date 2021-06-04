09578d6c83a208672e424c2cd35604bc890ebe7c
KevinRosado <kevin.rosado.s@uni.pe>
2021-06-03 21:56:49 -0500

    Cross Origin added

--- MemoAMautas/src/main/java/com/nixvoid/memoamautas/controller/DeckController.java
+++ MemoAMautas/src/main/java/com/nixvoid/memoamautas/controller/DeckController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/memo/decks", consumes = "application/json;charset=utf-8")
 public class DeckController {
